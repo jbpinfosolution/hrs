84ac0903ae5f72e172609f537be03606c93ea394
poojya saxena <p1s2.1999@gmail.com>
2022-10-17 13:41:27 +0530

    remove inline css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email:any;
+  password:any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+loginUser(){
+
+   if(this.email=="admin@gmail.com" && this.password=="12345678"){
+     window.alert("login successfully")
+   }
+   else{
+    window.alert("user unauthorized");
+   }
+}
 }
